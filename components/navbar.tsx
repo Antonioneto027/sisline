d8d2aa93e5a461f39f5410d80c68ae8d33f85a65
Antonioneto027 <antonionet.org@gmail.com>
2024-11-14 20:00:05 -0300

    Centralização da Navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -29,56 +29,53 @@ import Image from "next/image";
  
 
 export const Navbar = () => {
- 
-
   return (
-    <NextUINavbar className=" max-w-full mx-auto" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand as="li" className="gap-3 max-w-fit flex-between">
-          <NextLink className=" flex flex-between items-center gap-2 pr-10" href="/">
-              <Image src="/logo.png" alt="Logo" width={50} height={50} />
-              <p className="font-bold text-sm">SisLine</p>
+    <NextUINavbar className="max-w-full mx-auto px-4" position="sticky">
+      {/* Alinhamento inicial com logo */}
+      <NavbarContent className="basis-1/5 sm:basis-full flex items-center" justify="start">
+        <NavbarBrand as="li" className="gap-3">
+          <NextLink className="flex items-center gap-2 pr-10" href="/">
+            <Image src="/logo.png" alt="Logo" width={50} height={50} />
+            <p className="font-bold text-sm">SisLine</p>
           </NextLink>
         </NavbarBrand>
-        <ul className="hidden lg:flex gap-4 justify-start ml-2">
+        
+         <ul className="hidden lg:flex gap-4 ml-2 mx-auto">
           {siteConfig.navItems.map((item) => (
             <NavbarItem key={item.href}>
               <NextLink
                 className={clsx(
                   linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium",
+                  "data-[active=true]:text-primary data-[active=true]:font-medium"
                 )}
                 color="foreground"
                 href={item.href}
               >
                 {item.label}
               </NextLink>
-
-                
-              
             </NavbarItem>
           ))}
         </ul>
       </NavbarContent>
 
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
+       <NavbarContent
+        className="hidden sm:flex basis-1/5 gap-10 ml-40 justify-center items-center"
         justify="center"
       >
-       <NavbarItem className="hidden sm:flex gap-2">
-          <ThemeSwitch />
-        </NavbarItem>  
-        <NavbarItem className="hidden md:flex">
+        <NavbarItem className="hidden sm:flex gap-2">
         </NavbarItem>
+        <ThemeSwitch />
       </NavbarContent>
 
+      {/* Conteúdo alinhado à direita para telas menores */}
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
         <ThemeSwitch />
         <NavbarMenuToggle />
       </NavbarContent>
 
+      {/* Menu do Navbar para navegação móvel */}
       <NavbarMenu>
-         <div className="mx-4 mt-2 flex flex-col gap-2">
+        <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
               <Link
@@ -86,8 +83,8 @@ export const Navbar = () => {
                   index === 2
                     ? "primary"
                     : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
+                    ? "danger"
+                    : "foreground"
                 }
                 href="#"
                 size="lg"
