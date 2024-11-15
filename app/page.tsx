a63f72ac3e8ee96d066de0b70f3f51931721bee6
Antonioneto027 <antonionet.org@gmail.com>
2024-11-15 13:07:26 -0300

    Cover position and CSS configs

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,62 @@ import { AnimatedTextGradientTW } from "@/components/animatedText";
 
 export default function Home() {
   return (
-    <section className="font-sans flex content-center flex-col items-start gap-4 py-10 md:py-50">
-      <div className="inline-block  text-center content-center justify-start">
+    <section className="font-sans flex content-center flex-row justify-between  gap-4 py-10 md:py-50">
+      <div className="inline-block text-center content-center justify-start">
       <AnimatedTextGradientTW />
         <p className="text-4xl">Your Store Manager</p>
       </div>
+      <div>
+      <div className="relative flex justify-between flex-col">
+  {/* Item principal (posicionado relativo ao centro) */}
+  <Image
+    src="/cover/1.png"
+    className="relative mx-auto"
+    alt="Logo"
+    width={600}
+    height={600}
+  />
 
-    </section>
+  {/* Itens absolutos posicionados ao redor */}
+  <Image
+    src="/cover/2.png"
+    className="absolute animate-slideDown1 top-0 left-0"
+    alt="Logo"
+    width={100}
+    height={100}
+  />
+  <Image
+    src="/cover/3.png"
+    className="absolute animate-slideDown2 top-0 right-0"
+    alt="Logo"
+    width={100}
+    height={100}
+  />
+  <Image
+    src="/cover/4.png"
+    className="absolute animate-slideDown3 top-20 left-40"
+    alt="Logo"
+    width={100}
+    height={100}
+  />
+  <Image
+    src="/cover/5.png"
+    className="absolute animate-slideDown4 top-10 right-40"
+    alt="Logo"
+    width={100}
+    height={100}
+  />
+  <Image
+    src="/cover/6.png"
+    className="absolute animate-slideDown1 -top-10 left-60"
+    alt="Logo"
+    width={100}
+    height={100}
+  />
+</div>
+
+      </div>
+
+    </section>  
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/components/(button|code|input|kbd|link|listbox|navbar|snippet|toggle|ripple|spinner|divider|popover).js',
+    './app/modules/**/*.{js,ts,jsx,tsx,mdx}', // Adicionando módulo adicional
   ],
   theme: {
     extend: {
@@ -14,21 +15,62 @@ module.exports = {
         mono: ["var(--font-mono)"],
       },
       animation: {
-        "text-gradient": "text 5.0s linear infinite",
+        "text-gradient": "text 5s linear infinite",
+
+        'slideDown1': 'slideDown 2s ease-out forwards',
+        'slideDown2': 'slideDown2 2s ease-out forwards',
+        'slideDown3': 'slideDown3 2s ease-out forwards',    
+        'slideDown4': 'slideDown3 2s ease-out forwards',      
+        
       },
       keyframes: {
-        text: {
-          to: {
-            backgroundPosition: "200% center",
+        slideDown: {
+          '0%': {
+            transform: 'translateY(-25%)',  // Começando mais para cima
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'translateY(0)',  // Chega na posição original
+            opacity: '1',
+          },
+        },
+        slideDown2: {
+          '0%': {
+            transform: 'translateY(-40%)',  // Começa ainda mais acima
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'translateY(0)',  // Chega na posição original
+            opacity: '1',
+          },
+        },
+        slideDown3: {
+          '0%': {
+            transform: 'translateY(-35%)',  // Começa mais perto da posição final
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'translateY(0)',  // Chega na posição original
+            opacity: '1',
+          },
+          slideDown4: {
+            '0%': {
+              transform: 'translateY(-45%)',  // Começa ainda mais acima
+              opacity: '0',
+            },
+            '100%': {
+              transform: 'translateY(0)',  // Chega na posição original
+              opacity: '1',
+            },
           },
         },
       },
     },
   },
-  darkMode: "class",
+  darkMode: "class",  // Habilitando modo escuro
   plugins: [
     nextui({
-      defaultTheme: "light",
+      defaultTheme: "light", // Configurando o tema padrão do NextUI
     }),
   ],
 };
